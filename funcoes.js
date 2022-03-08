e8d57e3dbf1a57821929702b159d6605dfdfd4c4
carloscacho <carlosemilio.ac@gmail.com>
2022-03-08 17:31:57 -0400

    criando a função limpar

--- funcoes.js
+++ funcoes.js
@@ -18,14 +18,21 @@ function getNum(valor, pos){
     valor = Number(valor)
     if(valor>60){
         alert("O Valor é maior que 60")
+        limpar(pos)
     } else if(valor <= 0){
         alert("O valor não pode ser menor que 0")
+        limpar(pos)
     } else if (numDig.includes(valor)) {
         alert("O Valor já foi digitado!")
+        limpar(pos)
     }
     else{
         numDig[pos]= valor
     }
+}
+
+function limpar(pos){
+    document.getElementById(`num${pos}`).value = ""
 }
 
 function verificaAcertos(){
