803394bba4e37a4763dfb39d23dc0e0179ffe150
carloscacho <carlosemilio.ac@gmail.com>
2022-03-08 16:57:42 -0400

    criando as função importantes

---
+++ funcoes.js
@@ -0,0 +1,39 @@
+var numSort = []
+var numDig = []
+
+function sorteioNum(){
+    numSort=[]
+    do{
+        let sort = Math.ceil(Math.random()*60)        
+        if(!numSort.includes(sort))
+            numSort.push(sort) 
+          
+    }while(numSort.length<6)
+
+    document.getElementById("numsort").innerHTML = numSort        
+    verificaAcertos()
+}
+
+function getNum(valor, pos){
+    valor = Number(valor)
+    if(valor>60){
+        alert("O Valor é maior que 60")
+    } else if(valor <= 0){
+        alert("O valor não pode ser menor que 0")
+    } else if (numDig.includes(valor)) {
+        alert("O Valor já foi digitado!")
+    }
+    else{
+        numDig[pos]= valor
+    }
+}
+
+function verificaAcertos(){
+    let cont = 0
+    numDig.forEach(function(valor,index){
+        if(numSort.includes(valor)){
+            cont = cont+1
+        }
+    })
+    document.getElementById("total").innerText = cont
+}
