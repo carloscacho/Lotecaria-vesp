f7c5e4092f0b52db8515b55053e47cfccfdfc270
Carlos Emilio de A. Cacho <carlosemilio.ac@gmail.com>
2022-03-15 17:15:22 -0400

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -1,46 +1,54 @@
 var numSort = []
-var numDig = []
+var numDig=[]
 
 function sorteioNum(){
+    if(numDig.length < 6){
+        alert("Antes de sortear digite 6 numeros nos campos")
+    } else {
+        
     numSort=[]
     do{
         let sort = Math.ceil(Math.random()*60)        
         if(!numSort.includes(sort))
             numSort.push(sort) 
-          
+
     }while(numSort.length<6)
 
-    document.getElementById("numsort").innerHTML = numSort        
+    document.getElementById("numsort").innerHTML=numSort
     verificaAcertos()
+    }
+
 }
 
 function getNum(valor, pos){
     valor = Number(valor)
     if(valor>60){
-        alert("O Valor é maior que 60")
-        limpar(pos)
-    } else if(valor <= 0){
-        alert("O valor não pode ser menor que 0")
-        limpar(pos)
-    } else if (numDig.includes(valor)) {
-        alert("O Valor já foi digitado!")
-        limpar(pos)
+    alert("O valor é maior que 60")
+    }else if(valor<=0){
+        alert("O valor não pode ser menor que 0" )
+
+    }else if(numDig.includes(valor)){
+        alert("O valor já foi digitado!")
+
+
     }
+
     else{
-        numDig[pos]= valor
+       numDig[pos]=valor
     }
-}
 
-function limpar(pos){
-    document.getElementById(`num${pos}`).value = ""
-}
+    }
+    
+    function verificaAcertos(){
+        let cont = 0
+   numDig.forEach(function(valor,index){
+       if(numSort.includes(valor)){
+         cont=cont+1
 
-function verificaAcertos(){
-    let cont = 0
-    numDig.forEach(function(valor,index){
-        if(numSort.includes(valor)){
-            cont = cont+1
-        }
-    })
+       }
+
+   })
     document.getElementById("total").innerText = cont
-}
+   }
+    
+    
